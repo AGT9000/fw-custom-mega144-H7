273114d9b51616ad1338feaf09790bbe5b60d311
AGT9000 <84887759+AGT9000@users.noreply.github.com>
2025-09-18 14:59:01 +0700

    TPS ADC Channel

--- board_configuration.cpp
+++ board_configuration.cpp
@@ -19,10 +19,13 @@ static void customBoardDefaultConfiguration() {
 	engineConfiguration->adcVcc = 3.3f;
 	engineConfiguration->is_enabled_spi_3 = true;
 
-	engineConfiguration->map.sensor.hwChannel = EFI_ADC_4;
-	engineConfiguration->clt.adcChannel = EFI_ADC_6;
-	engineConfiguration->iat.adcChannel = EFI_ADC_7;
-	engineConfiguration->afr.hwChannel = EFI_ADC_14;
+	engineConfiguration->map.sensor.hwChannel = EFI_ADC_11;
+	engineConfiguration->clt.adcChannel = EFI_ADC_15;
+	engineConfiguration->iat.adcChannel = EFI_ADC_14;
+	engineConfiguration->afr.hwChannel = EFI_ADC_6;
+	//engineConfiguration->tps
+	engineConfiguration->tps1_1AdcChannel = EFI_ADC_0;
+	//engineConfiguration->tps1_2AdcChannel = tps2;
 
 	
 
