b4816954117dbbcb4ac8d3539879ac6467ecd7a6
GitHub git update Action <action@github.com>
2026-05-18 08:38:51 +0000

    Custom Auto-generated Configs
    [no ci]

--- generated/controllers/generated/signature_agtech-h7.h
+++ generated/controllers/generated/signature_agtech-h7.h
@@ -2,5 +2,5 @@
 // CHeaderConsumer was generated automatically by rusEFI tool config_definition-all.jar based on gen_config.sh by SignatureConsumer
 //
 
-#define SIGNATURE_HASH 1965858587
-#define TS_SIGNATURE "rusEFI main.2026.05.18.agtech-h7.1965858587"
+#define SIGNATURE_HASH 1021220278
+#define TS_SIGNATURE "rusEFI main.2026.05.18.agtech-h7.1021220278"
