4a82ea3cdc700955847be5b567cb93e29b74d314
AGT9000 <84887759+AGT9000@users.noreply.github.com>
2026-05-19 20:21:33 +0700

    Update board_configuration.cpp

--- board_configuration.cpp
+++ board_configuration.cpp
@@ -13,11 +13,28 @@ Gpio getWarningLedPin() {
 	return Gpio::Unassigned;
 }
 
+static void setupVbatt() {
+	// 5.6k high side/10k low side = 1.56 ratio divider
+	engineConfiguration->analogInputDividerCoefficient = 1.56f;
+
+	// 82k high side/10k low side = 9.2
+	engineConfiguration->vbattDividerCoeff = (92.0f / 10.0f);
+
+	// Battery sense on PA7
+	engineConfiguration->vbattAdcChannel = EFI_ADC_7;
+
+	engineConfiguration->adcVcc = 3.3f;
+}
+
 static void customBoardDefaultConfiguration() {
+
+	setupVbatt();
+	
     engineConfiguration->analogInputDividerCoefficient = 1.47f;
 
 	engineConfiguration->adcVcc = 3.3f;
 	engineConfiguration->is_enabled_spi_3 = false;
+	
 
 }
 
