76b1b1d48189b6125d52c669cce86fe35d5166f7
AGT9000 <84887759+AGT9000@users.noreply.github.com>
2026-05-19 20:38:29 +0700

    Update board_configuration.cpp

--- board_configuration.cpp
+++ board_configuration.cpp
@@ -23,12 +23,38 @@ static void setupVbatt() {
 	// Battery sense on PA7
 	engineConfiguration->vbattAdcChannel = EFI_ADC_7;
 
-	engineConfiguration->adcVcc = 3.3f;
+	
+}
+stsitc void setupCAN() {
+	
+	engineConfiguration->canTxPin = Gpio::D1;
+	engineConfiguration->canRxPin = Gpio::D0;
+	
+}
+
+static void setupDefaultSensorInputs() {
+	// trigger inputs
+	// Digital channel 1 as default - others not set
+	engineConfiguration->triggerInputPins[0] = Gpio::E8;
+	engineConfiguration->camInputs[0] = Gpio::Unassigned;
+
+	engineConfiguration->triggerInputPins[1] = Gpio::Unassigned;
+
+
+	engineConfiguration->clt.adcChannel = EFI_ADC_12;
+	engineConfiguration->iat.adcChannel = EFI_ADC_14;
+	engineConfiguration->tps1_1AdcChannel = EFI_ADC_0;
+	engineConfiguration->map.sensor.hwChannel = EFI_ADC_11;
+
+    // see also enableAemXSeries
+	// pin #28 WBO AFR "Analog Volt 10"
+	engineConfiguration->afr.hwChannel = EFI_ADC_10;
 }
 
 static void customBoardDefaultConfiguration() {
-
+    setupCAN();
 	setupVbatt();
+	setupDefaultSensorInputs();
 	
     engineConfiguration->analogInputDividerCoefficient = 1.47f;
 
